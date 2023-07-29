708695aee80ff8ca5f824a9bc0e3b91610c1ff38
Chien Nguyen <36409877+fankao@users.noreply.github.com>
2023-07-29 21:06:42 +0700

    Configuring integration between config server and config repo

--- src/main/java/com/polarbookshop/configservice/ConfigServiceApplication.java
+++ src/main/java/com/polarbookshop/configservice/ConfigServiceApplication.java
@@ -2,8 +2,10 @@ package com.polarbookshop.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer // Enable Spring Cloud Config server
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
